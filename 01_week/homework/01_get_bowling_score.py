b4abf8583cbdf80386d9a8ca07e524b8dfacedde
HEUMMAN <69341986+HEUMMAN@users.noreply.github.com>
2021-09-07 22:56:31 +0900

    9/7

--- 01_week/homework/01_get_bowling_score.py
+++ 01_week/homework/01_get_bowling_score.py
@@ -3,4 +3,4 @@ def get_bowling_score(s):
 
 
 assert get_bowling_score("9-8P72S9P-9S-P9-SS8") == 150
-assert get_bowling_score("SSSSSSSSSSSS") == 300
+assert get_bowling_score("SSSSSSSSSSSS") == 300 
