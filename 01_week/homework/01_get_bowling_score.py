f87e07be4ed0333bb62fe82134238a291d31e2f7
HEUMMAN <69341986+HEUMMAN@users.noreply.github.com>
2021-09-08 21:11:38 +0900

    9/8

--- 01_week/homework/01_get_bowling_score.py
+++ 01_week/homework/01_get_bowling_score.py
@@ -1,6 +1,31 @@
 def get_bowling_score(s):
+    result = 0
+    count = 0
+    frames = list(s)
+    print(frames)
+    for i in range(len(frames)):
+        if frames[i] == 'S':
+            result += 10
+            next_frame = frames[i+1]
+            if next_frame == 'S':
+                result += 10
+            elif next_frame == '-':
+                result += 0
+            else:
+                result += int(next_frame)
+            next_next_frame = frames[i+2]
+            if next_next_frame == 'S':
+                result += 10
+        print(result)
+
+
+
+
+
+
+    print(result)
     pass
 
 
 assert get_bowling_score("9-8P72S9P-9S-P9-SS8") == 150
-assert get_bowling_score("SSSSSSSSSSSS") == 300 
+assert get_bowling_score("SSSSSSSSSSSS") == 300
